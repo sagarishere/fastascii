b09e278c2a73a930a8af0e82fe71c5fd6ab0badb
sagarishere <5121817+sagarishere@users.noreply.github.com>
2024-12-10 00:22:36 +0200

    take out test prints

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module ascii
 
-go 1.23.1
+go 1.23.3
--- main.go
+++ main.go
@@ -1,55 +1,127 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"strings"
 )
 
-var myMap map[rune][]string
+var myMap map[rune][8]string
+var readyForPrint string
 
 func main() {
-	mybytes, _ := os.ReadFile("standard.txt")
-	myMap = fillMap(squeeze(strings.Split(strings.ReplaceAll(string(mybytes), "\\r\\n", "\\n"), "\n")))
-	myArgs := getArgs()
-	words := strings.Split(myArgs, "\\n")
-	printWords(words)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.HandleFunc("POST /ascii", Ascii)
+	http.HandleFunc("GET /ascii", AsciiMethodNotAllowed) // 405
+	http.HandleFunc("/", Home)
+	fmt.Println("Listening on port 8080... \033[34mhttp://localhost:8080/\033[0m")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func printWords(words []string) {
+func AsciiMethodNotAllowed(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		http.ServeFile(w, r, "./templates/404/404.html")
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	http.ServeFile(w, r, "./templates/home/home.html")
+}
+
+func Ascii(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	incoming := myStruct{}
+	err := json.Unmarshal(body, &incoming)
+	if err != nil {
+		fmt.Printf("\033[31mUnmarshal error: %v\033[0m\n", err)
+		w.WriteHeader(http.StatusBadRequest) // 400
+		return
+	}
+
+	// check if the struct is valid
+	if incoming.TypeOfAscii == "" || incoming.MakeAscii == "" {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		return
+	}
+
+	if incoming.TypeOfAscii != "standard" && incoming.TypeOfAscii != "shadow" && incoming.TypeOfAscii != "thinkertoy" {
+		w.WriteHeader(http.StatusNotFound) // 404
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	if !checkValidChars(incoming.MakeAscii) {
+		incoming.MakeAscii = "Invalid characters"
+	}
+	asciiResponse, err := giveAsccii(incoming.MakeAscii, incoming.TypeOfAscii)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError) // 500
+		return
+	}
+	jsonResponse, _ := json.Marshal(asciiResponse) // convert asciiResponse to json
+	fmt.Fprintf(w, string(jsonResponse))
+}
+
+func checkValidChars(myArgs string) bool {
+	for _, char := range myArgs {
+		if (char < 32 || char > 126) && char != '\n' {
+			return false
+		}
+	}
+	return true
+}
+
+type myStruct struct {
+	TypeOfAscii string `json:"type"`
+	MakeAscii   string `json:"text"`
+}
+
+func giveAsccii(myArgs, typeOfAscii string) (string, error) {
+	mybytes, err := os.ReadFile(typeOfAscii + ".txt")
+	if err != nil {
+		return "", err
+	}
+
+	myMap = fillMap(squeeze(strings.Split(strings.ReplaceAll(string(mybytes), "\r\n", "\n"), "\n")))
+	words := strings.Split(myArgs, "\n")
+	return giveInPrintFormatWords(words), nil
+}
+
+func giveInPrintFormatWords(words []string) (formatted string) {
 	for _, word := range words {
 		if word == "" {
-			fmt.Println()
+			formatted += "\n"
 		} else {
-			printWord(word)
+			formatted += (formatWord(word))
 		}
 	}
+	return
 }
 
-func printWord(word string) {
-	finalPrint := ""
+func formatWord(word string) (formatted string) {
 	for i := 0; i < 8; i++ {
 		for _, rune := range word {
-			finalPrint += myMap[rune][i]
+			formatted += myMap[rune][i]
 		}
-		finalPrint += "\n"
+		formatted += "\n"
 	}
-	fmt.Print(finalPrint)
+	return
 }
 
-func getArgs() string {
-	myArgs := os.Args[1:]
-	if len(myArgs) != 1 {
-		fmt.Println("Usage: go run main.go <words>")
-		os.Exit(1)
-	}
-	return myArgs[0]
-}
-
-func fillMap(squeezed []string) map[rune][]string {
-	result := make(map[rune][]string)
-	for i := 0; i < len(squeezed); i += 8 {
-		result[(rune(i/8 + ' '))] = squeezed[i : i+8]
+func fillMap(s []string) map[rune][8]string {
+	result := make(map[rune][8]string)
+	for i := 0; i < len(s); i += 8 {
+		result[rune(i/8+' ')] = [8]string{s[i], s[i+1], s[i+2], s[i+3], s[i+4], s[i+5], s[i+6], s[i+7]}
 	}
 	return result
 }
